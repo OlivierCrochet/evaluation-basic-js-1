56f40a621fef190dfd3bd555d109a5aace6bfe0d
Crochet Olivier <olivier.crochet@ynov.com>
2018-03-13 09:56:57 +0100

    Exerxice 1 correction

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,12 +1,20 @@
 var removeNegativeValue = function(values) {
   var output = [];
-  for ( let i = 0; i < values.length; i++) {
-    if (values[i] >= 0) {
-      output[i] = values[i]
-    }
+  var u = 0;
+  if (values == undefined){
+    return output
   }
+  else{
+    for ( let i =0; i < values.length; i++) {
+      if (values[i] >= 0) {
+        output[u] = values[i]
+        u +=1
+      }
+    }
   return output
+  }
 }
+
 
 module.exports = {
   title: 'Exercise 1',
